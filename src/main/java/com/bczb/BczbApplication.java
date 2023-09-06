19781442951bf8ec596da937787b95d524ec8ad4
LXQ <2823283343@qq.com>
2023-09-06 13:33:45 +0800

    初始化项目-李小桥

---
+++ src/main/java/com/bczb/BczbApplication.java
@@ -0,0 +1,13 @@
+package com.bczb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BczbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BczbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bczb/BczbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bczb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BczbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
