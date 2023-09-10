e1bcb7cfe29aa51a5e6077e534828c2c4b30a1dd
Hologlow <nlconlco@163.com>
2023-09-10 10:26:56 +0800

    第二次提交

--- src/main/java/com/bczb/controller/RecPanelController.java
+++ src/main/java/com/bczb/controller/RecPanelController.java
@@ -10,7 +10,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import javax.annotation.Resource;
 import java.util.ArrayList;
 
--- src/main/java/com/bczb/interceptor/AuthInterceptor.java
+++ src/main/java/com/bczb/interceptor/AuthInterceptor.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 public class AuthInterceptor implements HandlerInterceptor {
 
     @Override
-    public boolean preHandle(HttpServletRequest request,HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         
         String method = request.getMethod();
         if ("OPTIONS".equals(method)) {
@@ -19,8 +19,9 @@ public class AuthInterceptor implements HandlerInterceptor {
         }
         //从authorization中获取token
         String token = request.getHeader("Authorization");
-//        token = "eyJ0eXBlIjoiSldUIiwiYWxnbyI6IkhTMjU2IiwiYWxnIjoiSFM1MTIifQ.eyJleHBpcnlEYXRlIjoxNjk0MzQwNzE2MjYxLCJzdWIiOiJ1c2VyIiwiaWQiOjQ1LCJwb3dlciI6MCwiZXhwIjoxNjk0MzQwNzE2LCJqdGkiOiI3NmNmNDFmMC0xMGMyLTQ2N2QtYjUyMi0wNGRkMGY5MWIxZjQifQ.YbnZGEeCeGeXCTXwloUk3LqSHNAQKJGMMKbqOLFuCuZeKBBV1BAspkmFCIeAhbQZUQU-_sli5FoGyhbgE3DYHQ";
-//        System.out.println(request.getHeader("Authorization"));
+//        token = "eyJ0eXBlIjoiSldUIiwiYWxnbyI6IkhTMjU2IiwiYWxnIjoiSFM1MTIifQ.eyJleHBpcnlEYXRlIjoxNjk0MzQyOTQ1MDQxLCJzdWIiOiJ1c2VyIiwiaWQiOjQ1LCJwb3dlciI6MCwiZXhwIjoxNjk0MzQyOTQ1LCJqdGkiOiJlODZiYjBiMi1kYTQwLTQ4ZmItOWQyZi1iMmY4M2E5OTgxZWEifQ.jAy3XHidbLXx84xEaIlLB4JaGD_3NYscI4leVllzmxtBTIOoxgV9h0GGsxU4akKMdvG2uwnBZLq0kYq-XvzD1Q";
+        System.out.println("///////////  " + request.getHeader("Authorization"));
+        System.out.println("///////////  " + request.getMethod());
 //        System.out.println("///////////  " + token);
         //验证token
         if(!TokenUtils.isTokenCorrect(token)){
